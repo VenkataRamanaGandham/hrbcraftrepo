d63ad7a68f77e5281f278e30353e7e41aeffe097
venkata.gandham <venkata.gandham@pennywisesolutions.com>
2023-08-27 00:14:26 +0530

    Puhsing craft source code

---
+++ config/webhooks.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'disableAllWebhooks' => false,
+    'maxDepth' => 10,
+    'maxAttempts' => 3,
+    'initialDelay' => null,
+    'retryDelay' => 120,
+    'purgeDuration' => 604800, // 7 days
+];
